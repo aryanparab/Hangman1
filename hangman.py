c649f52457a8d475dceabd38067427191d12ba6c
Aryan Parab <66548981+aryanparab@users.noreply.github.com>
2020-06-30 12:30:03 +0530

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -16,21 +16,22 @@ def check(display,country):
 
     else:
         return True
-def guess(display,li,country,l):
-    yes=True
+def guess(display,lives,country,l):
+    yes = True
+
     inp=input().upper()
     for i in range(len(display)):
         if inp==country[i]:
             display[i]=inp
             yes=False
     if yes==True:
-        li=li-1
+        lives[0] -= 1
 
-    showdisplay(display,li)
+    showdisplay(display,lives[0])
     if check(display,country)==True :
         return True
-    elif li==0:
-        l=0
+    elif lives[0]==0:
+        l[0]=0
         return True
     else:
         return False
@@ -38,25 +39,29 @@ def guess(display,li,country,l):
 
 def playgame():
     country=getcountry()
-    l=len(country)
-    lives = 5
+    l=[len(country)]
+    lives = [5]
+
     display=[]
-    for i in range(l):
+    for i in range(len(country)):
         if country[i]!=' ':
             display.append("_")
         else:
             display.append(' ')
-    showdisplay(display,lives)
+    showdisplay(display,lives[0])
     ans=False
     while ans==False :
-        ans=guess(display,lives,country,l)
-    if l==0:
-        print('You Lost ')
+
+         ans=guess(display,lives,country,l)
+
+    if l[0]==0:
+        print('You Lost....The word Was : ' , country)
     else:
         print('You Win')
 
 while quit==False:
     inp=input('Enter to play or q to quit')
+
     if inp=='q':
         quit=True
     else:
